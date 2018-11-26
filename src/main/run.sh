58eb0394ab72e82b136d84648d82fc943ad68705
imans777 <iman.s_sani@yahoo.com>
2018-11-26 19:03:18 +0330

    update gitignore

--- src/main/run.sh
+++ src/main/run.sh
@@ -1,8 +1,5 @@
 set CLASSPATH="antlr-4.7.1-complete.jar:$CLASSPATH"
-rm *.class
-rm *.tokens
-rm Smoola*.java
-rm *.interp
+./clean.sh
 java -jar antlr-4.7.1-complete.jar Smoola.g4
 javac *.java
 java org.antlr.v4.gui.TestRig Smoola program < in.sml
